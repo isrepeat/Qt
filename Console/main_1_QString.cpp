ae2ab281dfbbeeee50e6d52b8e34397135025d0c
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-03-08 21:41:41 +0200

    Add main_2_remove_files_from_one_dir_to_another

--- Console/main_1_QString.cpp
+++ Console/main_1_QString.cpp
@@ -1,18 +1,18 @@
-#include <QtCore/QCoreApplication>
-#include <QString>
-#include <QDebug>
-
-
-int main(int argc, char* argv[]) {
-    QCoreApplication a(argc, argv);
-
-    
-    {// remove double quotes
-        QString stringWithDoubleQuotes = "\"Hello my world !!!\"";
-        auto list = stringWithDoubleQuotes.split('"');
-        auto res = stringWithDoubleQuotes.split('"').join("");
-        qDebug() << "res =" << list[1];
-    }
-
-    return a.exec();
-}
+//#include <QtCore/QCoreApplication>
+//#include <QString>
+//#include <QDebug>
+//
+//
+//int main(int argc, char* argv[]) {
+//    QCoreApplication a(argc, argv);
+//
+//    
+//    {// remove double quotes
+//        QString stringWithDoubleQuotes = "\"Hello my world !!!\"";
+//        auto list = stringWithDoubleQuotes.split('"');
+//        auto res = stringWithDoubleQuotes.split('"').join("");
+//        qDebug() << "res =" << list[1];
+//    }
+//
+//    return a.exec();
+//}
