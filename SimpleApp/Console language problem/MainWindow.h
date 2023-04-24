c2f23665261de24d49480173a346ef57544038ad
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-04-24 13:22:08 +0300

    Add "Layouts"

--- SimpleApp/Console language problem/MainWindow.h
+++ SimpleApp/Console language problem/MainWindow.h
@@ -1,30 +1,30 @@
-#pragma once
-//#include <QMainWindow>
-//#include <QPushButton>
-//#include <QTextEdit>
-//#include <QWidget>
-//#include <QLabel>
-#include <Windows.h>
-
-
-//class MainWindow : public QMainWindow
-//{
-//    Q_OBJECT
+//#pragma once
+////#include <QMainWindow>
+////#include <QPushButton>
+////#include <QTextEdit>
+////#include <QWidget>
+////#include <QLabel>
+//#include <Windows.h>
 //
-//public:
-//    MainWindow(QWidget* parent = 0);
-//    ~MainWindow() = default;
 //
-//private:
+////class MainWindow : public QMainWindow
+////{
+////    Q_OBJECT
+////
+////public:
+////    MainWindow(QWidget* parent = 0);
+////    ~MainWindow() = default;
+////
+////private:
+////
+////    QLabel* label;
+////    QTextEdit* textEdit;
+////    QPushButton* pushBtn;
+////};
 //
-//    QLabel* label;
-//    QTextEdit* textEdit;
-//    QPushButton* pushBtn;
-//};
-
-
-void CreateChildProcess(void);
-void WriteToPipe(void);
-void ReadFromPipe(void);
-void ErrorExit(PTSTR);
-void Test();
+//
+//void CreateChildProcess(void);
+//void WriteToPipe(void);
+//void ReadFromPipe(void);
+//void ErrorExit(PTSTR);
+//void Test();
