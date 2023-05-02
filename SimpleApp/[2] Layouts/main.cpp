aa1283933128d6cef461d9310bb81279d43fe611
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-05-02 17:17:24 +0300

    ["Layouts"] Add ClearLayout()

--- SimpleApp/[2] Layouts/main.cpp
+++ SimpleApp/[2] Layouts/main.cpp
@@ -1,13 +1,13 @@
-//#include <QApplication>
-//#include "MainWindow.h"
-//
-//int main(int argc, char **argv)
-//{
-//    QApplication app(argc, argv);
-//    
-//    auto window = new MainWindow();
-//    window->resize(800,600);
-//    window->show();
-//
-//    return app.exec();
-//}
+#include <QApplication>
+#include "MainWindow.h"
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    
+    auto window = new MainWindow();
+    window->resize(800,600);
+    window->show();
+
+    return app.exec();
+}
--- SimpleApp/[3] Connect expired ptr/RemoteDesktopWindow.h
+++ SimpleApp/[3] Connect expired ptr/RemoteDesktopWindow.h
@@ -1,23 +1,23 @@
-#pragma once
-#include <QMainWindow>
-#include <QPushButton>
-#include <QTextEdit>
-#include <QWidget>
-#include <QLabel>
-
-class RemoteDesktopWindow : public QWidget {
-    Q_OBJECT
-
-public:
-    RemoteDesktopWindow(QWidget* parent = nullptr);
-    ~RemoteDesktopWindow() = default;
-
-    QSize GetSize();
-    void SetSize(QSize size);
-
-signals:
-    void NewSize(QSize newSize);
-
-private:
-    QSize currentSize = { 100, 100 };
-};
+//#pragma once
+//#include <QMainWindow>
+//#include <QPushButton>
+//#include <QTextEdit>
+//#include <QWidget>
+//#include <QLabel>
+//
+//class RemoteDesktopWindow : public QWidget {
+//    Q_OBJECT
+//
+//public:
+//    RemoteDesktopWindow(QWidget* parent = nullptr);
+//    ~RemoteDesktopWindow() = default;
+//
+//    QSize GetSize();
+//    void SetSize(QSize size);
+//
+//signals:
+//    void NewSize(QSize newSize);
+//
+//private:
+//    QSize currentSize = { 100, 100 };
+//};
